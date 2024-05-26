560fb8f9872dff9986f3da289b8e46d09403d45a
hahaha98757 <116276330+hahaha98757@users.noreply.github.com>
2024-05-26 21:15:44 +0900

    Add files via upload

---
+++ src/main/java/kr/hahaha98757/zombiesaddon/commands/CommandInfo.java
@@ -0,0 +1,107 @@
+package kr.hahaha98757.zombiesaddon.commands;
+
+import java.util.List;
+
+import com.mojang.realmsclient.gui.ChatFormatting;
+
+import kr.hahaha98757.zombiesaddon.Keybinds;
+import kr.hahaha98757.zombiesaddon.ZombiesAddon;
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.ChatComponentText;
+
+public class CommandInfo extends CommandBase {
+	public static final String LINE = "\u00A7e-----------------------------------------------------";
+
+	@Override
+	public String getCommandName() {
+		return "info";
+	}
+
+	@Override
+	public int getRequiredPermissionLevel() {
+		return 0;
+	}
+
+	@Override
+	public String getCommandUsage(ICommandSender sender) {
+		return "\u00A7cUsage: /info [mod name]";
+	}
+
+	@Override
+	public void processCommand(ICommandSender sender, String[] args) {
+		if (!(sender instanceof EntityPlayer)) {
+			return;
+		}
+		EntityPlayer player = (EntityPlayer) sender;
+		if (args.length == 0) {
+			player.addChatComponentMessage(new ChatComponentText(LINE + "\nMod name: " + ZombiesAddon.NAME
+					+ "\nVersion: " + ZombiesAddon.VERSION
+					+ "\nMod list: Cornering, Block Alarm, DPS Counter, NOTLAST, Auto Splits, ZSV, SLA, Advanced SLA, Auto Rejoin, Powerup Alarm, Boss Alarm, Grow ESP, and Gstep Guide.\nInfo: A mod that helps playing hypixel zombies.\nCommand tips: Use /info [Cornering|BlockAlarm|DPSCounter|NOTLAST|AutoSplits|ZSV|SLA|AdvancedSLA|AutoRejoin|PowerupAlarm|BossAlarm|GrowESP|GstepGuide] to view mod's description.\nURL: https://github.com/hahaha98757/ZombiesAddon\n"
+					+ LINE));
+			return;
+		}
+
+		String text;
+
+		switch (args[0]) {
+		case "Cornering":
+			text = "Name: Cornering\nVersion: 1.1.1\nCode by: syeyoung and thamid\nInfo: Toggles the visibility of players nearby.\nCommands: /cornering\nKeybinds: " + Keybinds.toggleCornering.getKeyDescription() + "\nConfig: Used";
+			break;
+		case "BlockAlarm":
+			text = "Name: Block Alarm\nVersion: 1.1.1\nCode by: syeyoung\nInfo: If you survived alone, show \"BLOCK\" on display.\nKeybinds: " + Keybinds.toggleBlockAlarm.getKeyDescription();
+			break;
+		case "DPSCounter":
+			text = "Name: DPS Counter\nVersion: 1.3\nCode by: thamid\nInfo: Counts your party's DPS.";
+			break;
+		case "NotLast":
+			text = "Name: Not Last\nVersion: 1.2\nCode by: thamid\nInfo: Shows who is kill the last in every round.";
+			break;
+		case "AutoSplits":
+			text = "Name: Auto Splits\nVersion: 1.0\nCode by: thamid\nInfo: Run timer at start of round.";
+			break;
+		case "WaveDelays":
+			text = "Name: Wave Delays\nCode by: hahaha98757\nInfo: Show wave delays.\nConfig: Used";
+			break;
+		case "GrowGuide":
+			text = "Name: Grow Guide\nCode by: hahaha98757\nInfo: Displays the blob's grow time.";
+			break;
+		case "ZSV":
+			text = "Name: ZSV(Zombies Strat Viewer)\nVersion: 1.1.1\nCode by: syeyoung\nInfo: Displays the text in https://pastebin.com/ on the in-game screen.\nCommands: /ZSV and /ZSVLines";
+			break;
+		case "SLA":
+			text = "Name: SLA(Spawn Limit Action)\nVersion: 1.3(SLAv1), 1.1.1(SLAv2)\nCode by: thamid(SLAv1), Stachelbeere1248(SLAv2)\nInfo: Display number of windows where zombies can spawn.\nCommands: /SLA\nConfig: Used";
+			break;
+		case "AutoRejoin":
+			text = "Name: Auto Rejoin\nVersion: 1.1\nCode by: thamid\nInfo: Automatically rejoin the Zombies game.\nKeybinds: " + Keybinds.toggleAutoRejoin.getKeyDescription() + "\nConfig: Used";
+			break;
+		case "PowerupAlarm":
+			text = "Name: Powerup Alarm\nCode by: hahaha98757\nInfo: You will receive an alarm when a powerup is spawned. You will know when powerup spawn and despawn.\nCommands: /powerupAlarm, /pow";
+			break;
+		case "BossAlarm":
+			text = "Name: Boss Alarm\nCode by: hahaha98757\nInfo: You will know the area where the boss spawned.";
+			break;
+		case "GrowESP":
+			text = "Name: Grow ESP\nCode by: Dreaght\nInfo: Shows the arithmetic mean of the size of the slimes for growing.";
+			break;
+		case "GstepGuide":
+			text = "Name: Gstep Guide\nCode by: hahaha98757\nInfo: Shows hitbox of armor stand in Gstep.";
+			break;
+		default:
+			player.addChatComponentMessage(new ChatComponentText(getCommandUsage(null)));
+			return;
+		}
+		player.addChatComponentMessage(new ChatComponentText(LINE + "\n" + text + "\n" + LINE));
+	}
+
+	@Override
+	public List<String> addTabCompletionOptions(ICommandSender sender, String[] args, BlockPos pos) {
+		if (args.length == 1) {
+			return getListOfStringsMatchingLastWord(args,
+					new String[] { "Cornering", "BlockAlarm", "DPSCounter", "NotLast", "AutoSplits", "WaveDelays", "GrowGuide", "ZSV", "SLA", "AutoRejoin", "PowerupAlarm", "BossAlarm", "GrowESP", "GstepGuide" });
+		}
+		return null;
+	}
+}
---
+++ src/main/java/kr/hahaha98757/zombiesaddon/commands/CommandLog.java
@@ -0,0 +1,251 @@
+package kr.hahaha98757.zombiesaddon.commands;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.mojang.realmsclient.gui.ChatFormatting;
+
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.ChatComponentText;
+
+public class CommandLog extends CommandBase {
+
+	public String getCommandName() {
+		return "updatelog";
+	}
+
+	public int getRequiredPermissionLevel() {
+		return 0;
+	}
+
+	public String getCommandUsage(ICommandSender sender) {
+		return "\u00A7cUsage: /updatelog <version>";
+	}
+
+	public List<String> getCommandAliases() {
+		return Arrays.<String>asList(new String[] { "log" });
+	}
+
+	public void processCommand(ICommandSender sender, String[] args) {
+		if (!(sender instanceof EntityPlayer)) {
+			return;
+		}
+		EntityPlayer player = (EntityPlayer) sender;
+		if (args.length == 0) {
+			player.addChatComponentMessage(new ChatComponentText(getCommandUsage(null)));
+			return;
+		}
+
+		String text;
+
+		switch (args[0]) {
+		case "1.1.0":
+			text = "Version: 1.1.0\nLog: Removed ZHF mod.";
+			break;
+		case "1.2.0":
+			text = "Version: 1.2.0\nLog: Added NOTLAST mod.";
+			break;
+		case "1.2.1":
+			text = "Version: 1.2.1\nLog: Added /updatelog command. /info command fixed.";
+			break;
+		case "1.3.0":
+			text = "Version: 1.3.0\nLog: Added Auto Splits mod.";
+			break;
+		case "1.4.0":
+			text = "Version: 1.4.0\nLog: Added ZSV and SLA mod.";
+			break;
+		case "1.4.1":
+			text = "Version: 1.4.1\nLog: Fixed mod's message.";
+			break;
+		case "1.4.2":
+			text = "Version: 1.4.2\nLog: Fixed bug. Block Alarm fixed. Added /blockAlarm command.";
+			break;
+		case "1.4.3":
+			text = "Version: 1.4.3\nLog: Fixed bug. You can use Block Alarm in Korean.";
+			break;
+		case "1.4.4":
+			text = "Version: 1.4.4\nLog: Fixed Block Alarm.";
+			break;
+		case "1.4.5":
+			text = "Version: 1.4.5\nLog: Fixed Block Alarm.";
+			break;
+		case "1.5.0":
+			text = "Version: 1.5.0\nLog: Added Auto Rejoin mod. Fixed bug. Fixed AutoSplits.";
+			break;
+		case "1.5.1":
+			text = "Version: 1.5.1\nLog: Fixed bug.";
+			break;
+		case "1.5.2":
+			text = "Version: 1.5.2\nLog: Fixed bug.";
+			break;
+		case "1.6.0":
+			text = "Version: 1.6.0\nLog: Remake /setMap, /setstrat, /setlines commands name to /SLA, /ZSV, /ZSVLines. Remove /blockAlarm command. Added /cornering command. Added config for Cornering and BlockAlarm.";
+			break;
+		case "1.6.1":
+			text = "Version: 1.6.1\nLog: Fixed Block Alarm.";
+			break;
+		case "1.6.2":
+			text = "Version: 1.6.2\nLog: Fixed NOTLAST and Auto Rejoin. Fixed bug.";
+			break;
+		case "1.6.3":
+			text = "Version: 1.6.3\nLog: Fixed bug.";
+			break;
+		case "1.6.4":
+			text = "Version: 1.6.4\nLog: Fixed bug.";
+			break;
+		case "1.6.5":
+			text = "Version: 1.6.5\nLog: Fixed bug.";
+			break;
+		case "1.7.0":
+			text = "Version: 1.7.0\nLog: Fixed AutoSplits. Fixed /ZSV, /SLA command. Fixed AutoRejoin. Added config for AutoSplits.";
+			break;
+		case "1.7.1":
+			text = "Version: 1.7.1\nLog: Fixed bug.";
+			break;
+		case "1.7.2":
+			text = "Version: 1.7.2\nLog: Fixed bug.";
+			break;
+		case "1.7.3":
+			text = "Version: 1.7.3\nLog: Fixed bug.";
+			break;
+		case "1.7.4":
+			text = "Version: 1.7.4\nLog: Fixed bug.";
+			break;
+		case "1.7.5":
+			text = "Version: 1.7.5\nLog: Fixed bug.";
+			break;
+		case "1.7.6":
+			text = "Version: 1.7.6\nLog: Fixed bug.";
+			break;
+		case "1.8.0":
+			text = "Version: 1.8.0\nLog: Added Powerup Alarm. Fixed bugs. Added config for ZombiesAddon. Fixed Auto Splits.";
+			break;
+		case "1.8.1":
+			text = "Version: 1.8.1\nLog: Fixed Powerup Alarm. Fixed bugs. Added config for SLA.";
+			break;
+		case "1.8.2":
+			text = "Version: 1.8.2\nLog: Fixed bugs.";
+			break;
+		case "1.8.3":
+			text = "Version: 1.8.3\nLog: Fixed bugs.";
+			break;
+		case "1.8.4":
+			text = "Version: 1.8.4\nLog: Fixed bugs. Fixed Auto Splits.";
+			break;
+		case "1.8.5":
+			text = "Version: 1.8.5\nLog: Fixed bugs. Fixed CommandInfo. Added /zombies Command for Zombies Overlay.";
+			break;
+		case "1.8.6":
+			text = "Version: 1.8.6\nLog: Fixed bugs. Removed /zombies command. Added config for Zombies Overlay. Fixed Powerup Alarm.";
+			break;
+		case "1.8.7":
+			text = "Version: 1.8.7\nLog: Fixed bugs.";
+			break;
+		case "1.8.8":
+			text = "Version: 1.8.8\nLog: Fixed bugs. Added Boss Wave Alarm for AutoSplits.";
+			break;
+		case "1.8.9":
+			text = "Version: 1.8.9\nLog: Fixed bugs.";
+			break;
+		case "1.9.0":
+			text = "Version: 1.9.0\nLog: Fixed bugs. Added Boss Alarm. Fixed forge command bug.";
+			break;
+		case "1.9.1":
+			text = "Version: 1.9.1\nLog: Fixed bugs.";
+			break;
+		case "1.9.2":
+			text = "Version: 1.9.2\nLog: Fixed bugs.";
+			break;
+		case "1.10.0":
+			text = "Version: 1.10.0\nLog: Changed package. Code Reorganization. Added Lrod Order. Added Advanced SLA. Fixed command.";
+			break;
+		case "1.10.1":
+			text = "version: 1.10.1\nLog: Fixed bugs.";
+			break;
+		case "1.10.2":
+			text = "version: 1.10.2\nLog: Added Hologram Bug Generator.";
+			break;
+		case "1.10.3":
+			text = "version: 1.10.3\nLog: Fixed bugs.";
+			break;
+		case "1.11.0":
+			text = "versoin: 1.11.0\nLog: Added Health Indicator for Block Alarm. Rename Hologram Bug Generator to Hologram Remover. Removed rev and dead for Block Alarm.";
+			break;
+		case "1.12.0":
+			text = "version: 1.12.0\nLog: Added Grow Guide for Auto Splits. Fixed bugs. Fixed config. Added mod version on the display. Config reset when mod update. Added option which default value of mod be set.";
+			break;
+		case "1.12.1":
+			text = "version: 1.12.1\nLog: Removed Hologram Remover. Added update checker.";
+			break;
+		case "1.12.2":
+			text = "version: 1.12.2\nLog: Fixed update checker. Fixed /info command.";
+			break;
+		case "1.12.3":
+			text = "version: 1.12.3\nLog: Fixed bugs. Fixed update checker.";
+			break;
+		case "1.13.0":
+			text = "version: 1.13.0\nLog: Fixed bugs. Added Grow ESP. Wave delay of SST is off when use with SST.";
+			break;
+		case "1.13.1":
+			text = "version: 1.13.1\nLog: Fixed Grow ESP.";
+			break;
+		case "1.14.0-pre1":
+			text = "version: 1.14.0-pre1\nLog: Added Gstep Guide. Fixed DPS Counter, NOTLAST, Auto Splits, and Powerup Alarm. Fixed bugs.";
+			break;
+		case "1.14.0-pre2":
+			text = "version: 1.14.0-pre2\nLog: Fixed bugs. Fixed Gstep Guide.";
+			break;
+		case "1.14.0-pre3":
+			text = "version: 1.14.0-pre3\nLog: Fixed bugs.";
+			break;
+		case "1.14.0-pre4":
+			text = "version: 1.14.0-pre4\nLog: Fixed bugs.";
+			break;
+		case "1.14.0":
+			text = "version: 1.14.0\nLog: Fixed bugs. Timer of Zombies Utils is off when use with Zombies Utils. Removed Health Indicator for Block Alarm.";
+			break;
+		case "1.15.0":
+			text = "version: 1.15.0\nLog: Fixed bugs. Added Play Sound for Auto Splits.";
+			break;
+		case "1.15.1":
+			text = "version: 1.15.1\nLog: Fixed bugs. Removed Countdown.";
+			break;
+		case "1.16.0-pre1":
+			text = "version: 1.16.0-pre1\nLog: Fixed bugs. Removed Lrod Order. Added RL-mode for Auto Splits. Fixed Update Checker. Added Extra Setting. Fixed Wave Delays. Removed config for Powerup Alarm.";
+			break;
+		case "1.16.0-pre2":
+			text = "version: 1.16.0-pre2\nLog: Fixed bugs.";
+			break;
+		case "1.16.0":
+			text = "version: 1.16.0\nLog: Fixed bugs. Renamed Extra Setting.";
+			break;
+		case "1.17.0-pre1":
+			text = "version: 1.17.0-pre1\nLog: Fixed bugs. Fixed Auto Splits. Renamed NOTLAST. Fixed config. Fixed Wave Delays. Fixed Not Last.";
+			break;
+		case "1.17.0-pre2":
+			text = "version: 1.17.0-pre2\nLog: Fixed bugs.";
+			break;
+		default:
+			player.addChatComponentMessage(new ChatComponentText(getCommandUsage(null)));
+			return;
+		}
+		player.addChatComponentMessage(new ChatComponentText(CommandInfo.LINE + "\n" + text + "\n" + CommandInfo.LINE));
+	}
+
+	public List<String> addTabCompletionOptions(ICommandSender sender, String[] args, BlockPos pos) {
+		if (args.length == 1) {
+			return getListOfStringsMatchingLastWord(args,
+					new String[] { "1.1.0", "1.2.0", "1.2.1", "1.3.0", "1.4.0", "1.4.1", "1.4.2", "1.4.3", "1.4.4",
+							"1.4.5", "1.5.0", "1.5.1", "1.5.2", "1.6.0", "1.6.1", "1.6.2", "1.6.3", "1.6.4", "1.6.5",
+							"1.7.0", "1.7.1", "1.7.2", "1.7.3", "1.7.4", "1.7.5", "1.7.6", "1.8.0", "1.8.1", "1.8.2",
+							"1.8.3", "1.8.4", "1.8.5", "1.8.6", "1.8.7", "1.8.8", "1.8.9", "1.9.0", "1.9.1", "1.9.2",
+							"1.10.0", "1.10.1", "1.10.2", "1.10.3", "1.11.0", "1.12.0", "1.12.1", "1.12.2", "1.12.3",
+							"1.13.0", "1.13.1", "1.14.0-pre1", "1.14.0-pre2", "1.14.0-pre3", "1.14.0-pre4", "1.14.0",
+							"1.15.0", "1.15.1", "1.16.0-pre1", "1.16.0-pre2", "1.16.0", "1.17.0-pre1", "1.17.0-pre2" });
+		}
+		return null;
+	}
+}
