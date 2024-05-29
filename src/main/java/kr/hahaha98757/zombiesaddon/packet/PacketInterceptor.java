dc80a85bb95e417cf35bfe7be6f589e265508c31
hahaha98757 <116276330+hahaha98757@users.noreply.github.com>
2024-05-29 18:38:17 +0900

    Add files via upload

---
+++ src/main/java/kr/hahaha98757/zombiesaddon/packet/PacketInterceptor.java
@@ -0,0 +1,16 @@
+package kr.hahaha98757.zombiesaddon.packet;
+
+import net.minecraft.network.Packet;
+
+import java.util.function.Consumer;
+
+@FunctionalInterface
+public interface PacketInterceptor extends Consumer<Packet<?>> {
+
+	void intercept(Packet<?> packet);
+
+	default void accept(Packet<?> packet) {
+		intercept(packet);
+	}
+
+}
