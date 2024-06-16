8bed03a8e6f0a059b06689726e3cf7b8a9eba7ad
hahaha98757 <116276330+hahaha98757@users.noreply.github.com>
2024-06-16 16:59:48 +0900

    Add files via upload

---
+++ src/main/java/kr/hahaha98757/zombiesaddon/handler/ClientConnectHandler.java
@@ -0,0 +1,18 @@
+package kr.hahaha98757.zombiesaddon.handler;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelPipeline;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.network.FMLNetworkEvent;
+
+public abstract class ClientConnectHandler {
+
+	@SubscribeEvent
+	public void onClientConnectedToServer(FMLNetworkEvent.ClientConnectedToServerEvent event) {
+		ChannelPipeline pipeline = event.manager.channel().pipeline();
+		pipeline.addBefore("packet_handler", "zombies_auto_splits", createChannelHandler());
+	}
+
+	protected abstract ChannelHandler createChannelHandler();
+
+}
