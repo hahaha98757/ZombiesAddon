9c84a825bf199e01bff4be7e03e2afd1fceaba83
hahaha98757 <116276330+hahaha98757@users.noreply.github.com>
2024-06-29 18:16:09 +0900

    Add files via upload

---
+++ src/main/java/kr/hahaha98757/zombiesaddon/config/ZombiesAddonGuiConfig.java
@@ -0,0 +1,36 @@
+package kr.hahaha98757.zombiesaddon.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import kr.hahaha98757.zombiesaddon.ZombiesAddon;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.common.config.ConfigElement;
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.fml.client.config.DummyConfigElement;
+import net.minecraftforge.fml.client.config.GuiConfig;
+import net.minecraftforge.fml.client.config.IConfigElement;
+
+public class ZombiesAddonGuiConfig extends GuiConfig {
+
+	public ZombiesAddonGuiConfig(GuiScreen parent) {
+		super(parent, getElements(), ZombiesAddon.MODID, false, false, "Zombies Addon Configuration");
+	}
+
+	private static List<IConfigElement> getElements() {
+		List<IConfigElement> list = new ArrayList<>();
+
+		list.add(new DummyConfigElement.DummyCategoryElement("Zombies Addon", "",
+				new ArrayList<>(ZombiesAddonConfig.zombiesAddon.values())));
+		list.add(new DummyConfigElement.DummyCategoryElement("Wave Delays", "",
+				new ArrayList<>(ZombiesAddonConfig.waveDelays.values())));
+		list.add(new DummyConfigElement.DummyCategoryElement("SLA", "",
+				new ArrayList<>(ZombiesAddonConfig.sla.values())));
+		list.add(new DummyConfigElement.DummyCategoryElement("Trackers", "",
+				new ArrayList<>(ZombiesAddonConfig.trackers.values())));
+		list.add(new DummyConfigElement.DummyCategoryElement("Others", "",
+				new ArrayList<>(ZombiesAddonConfig.others.values())));
+		return list;
+
+	}
+}
---
+++ src/main/java/kr/hahaha98757/zombiesaddon/listeners/PlayerTrackerListener.java
@@ -0,0 +1,92 @@
+package kr.hahaha98757.zombiesaddon.listeners;
+
+import org.lwjgl.opengl.GL11;
+
+import kr.hahaha98757.zombiesaddon.commands.CommandCornering;
+import kr.hahaha98757.zombiesaddon.config.ZombiesAddonConfig;
+import kr.hahaha98757.zombiesaddon.utils.GameDetect;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.RenderGlobal;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraftforge.client.event.RenderWorldLastEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+public class PlayerTrackerListener {
+
+	public static double range = 2.5;
+
+	@SubscribeEvent
+	public void onRenderWorldLast(RenderWorldLastEvent event) {
+		if (!ZombiesAddonConfig.enableMod) {
+			return;
+		}
+		if (!ZombiesAddonConfig.togglePlayerTracker) {
+			return;
+		}
+		if (!GameDetect.isZombiesGame()) {
+			return;
+		}
+		for (EntityPlayer player : Minecraft.getMinecraft().theWorld.playerEntities) {
+
+			if (player == Minecraft.getMinecraft().thePlayer) {
+				continue;
+			}
+
+			if (withinDistance(player)) {
+				continue;
+			}
+			
+			if (player.getName().equals(GameDetect.getPlayerNames()[1])) {
+				GL11.glColor4f(0.3F, 1.0F, 0.3F, 1.0F);
+			} else if (player.getName().equals(GameDetect.getPlayerNames()[2])) {
+				GL11.glColor4f(0.3F, 1.0F, 1.0F, 1.0F);
+			} else if (player.getName().equals(GameDetect.getPlayerNames()[3])) {
+				GL11.glColor4f(1.0F, 0.6F, 0.0F, 1.0F);
+			} else {
+				GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+			}
+			
+			RenderManager renderManager = Minecraft.getMinecraft().getRenderManager();
+			AxisAlignedBB boundingBox = player.getEntityBoundingBox();
+
+			GlStateManager.enableBlend();
+			GlStateManager.tryBlendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ONE,
+					GL11.GL_ZERO);
+
+			GlStateManager.disableTexture2D();
+			GlStateManager.disableLighting();
+			GlStateManager.disableCull();
+			GlStateManager.disableDepth();
+
+			GlStateManager.pushMatrix();
+			GlStateManager.translate(-renderManager.viewerPosX, -renderManager.viewerPosY, -renderManager.viewerPosZ);
+
+			AxisAlignedBB adjustedBoundingBox = new AxisAlignedBB(boundingBox.minX, boundingBox.minY, boundingBox.minZ,
+					boundingBox.maxX, boundingBox.maxY, boundingBox.maxZ);
+
+			RenderGlobal.drawSelectionBoundingBox(adjustedBoundingBox);
+
+			GlStateManager.popMatrix();
+
+			GlStateManager.enableDepth();
+			GlStateManager.enableCull();
+			GlStateManager.enableLighting();
+			GlStateManager.enableTexture2D();
+			GlStateManager.disableBlend();
+		}
+	}
+
+	private static boolean withinDistance(EntityPlayer other) {
+		return getDistanceSquared(other) < Math.pow(range, 2);
+	}
+
+	private static double getDistanceSquared(EntityPlayer other) {
+		EntityPlayerSP playerSP = Minecraft.getMinecraft().thePlayer;
+		return Math.pow(playerSP.posX - other.posX, 2) + Math.pow(playerSP.posZ - other.posZ, 2);
+	}
+}
